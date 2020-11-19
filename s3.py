d311b531fb2bcd0a93500f56b6a16e741bb29428
IkerDC <iker.diaz01@estudiant.upf.edu>
2020-11-19 20:38:19 +0100

    class and ex1 in process

--- s3.py
+++ s3.py
@@ -1,4 +1,22 @@
+import os
+
+class Container:
+    def __init__(self):
+        self.video_file = ""
+        self.mono_audio = ""
+        self.low_audio = ""
+        self.subtitle = ""
+
+    def create_BBB_container(self):
+        #Cut 1 min
+        cut1 =  "ffmpeg -ss 00:00 -i " + self.video_file+ ".mp4 -to 01:00 -c copy cut_"+self.video_file
+        cut2 = "ffmpeg -i " + self.video_file + ".mp4 -ss 00:00 -to 01:00 -c copy cut_" +self.video_file
+        #Take the mono
+        mono1 = "ffmpeg -i cut_" +self.video_file+ ".mp4 -ac 1 mono_" +self.video_file
+        mono2 = "ffmpeg -i mono_"+self.video_file+" -vn -acodec copy mono.mp3"
+        #TAKE LOWER BIT RATE
 
 
 
-print("weiiiiiiii ua sap")
+a = Container()
+a.video_file = "bbb.mp4"
