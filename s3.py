d661cd45f3fd320fc02f48fc8c3e094ad7df6e10
IkerDC <iker.diaz01@estudiant.upf.edu>
2020-11-21 12:39:25 +0100

    broadcast done

--- s3.py
+++ s3.py
@@ -1,11 +1,29 @@
 import os
 
 class Container:
-    def __init__(self):
-        self.video_file = ""
-        self.mono_audio = ""
-        self.low_audio = ""
-        self.subtitle = ""
+    def __init__(self, filename):
+        self.video_file = filename
+        self.mono_audio = None
+        self.low_audio = None
+        self.subtitle = None
+        self.tracks = self.read_stream()
+
+    def read_stream(self):
+        cmd = "ffmpeg -i " + self.video_file + " 2> metadata.txt"
+        os.system(cmd)
+        retrive = ["Stream #0:0(und)", "Stream #0:1(und)", "Stream #0:2(und)"]
+        data = []
+        with open('metadata.txt', 'rt') as myfile:
+            contents = myfile.read()  # Read the entire file to a string
+            for value in retrive:
+                if (1 + contents.find(value)):  # If it doesnt exist it returns -1 = 1-1 = 0 = False
+                    pos = contents.find(value)
+                    start_pos = pos + len(value) + 9 # For the : Video:  or : Audio: part with len 9 always
+                    end_pos = contents.find(' ', start_pos)  # Find the next space, so where codec ends
+                    result = contents[start_pos:end_pos]  # Read from ...= to the next line
+                    data.append(result)
+        os.remove("metadata.txt")  # Remove the file we created
+        return data
 
     def create_BBB_container(self):
         #Cut 1 min
@@ -16,7 +34,30 @@ class Container:
         mono2 = "ffmpeg -i mono_"+self.video_file+" -vn -acodec copy mono.mp3"
         #TAKE LOWER BIT RATE
 
+    def broadcasting(self, brd_std):
+        possible_std = []
+        print("Streams of the video:", self.tracks)
+        for key in brd_std:
+            if(all(elem in brd_std[key]  for elem in self.tracks)):
+                possible_std.append(key)
+                print("Possible standart:", key)
+        if not possible_std:
+            print("ERROR: The streams give are not in any standart, or not all the stream not compatible with one sam standart")
+        return  possible_std
 
 
-a = Container()
-a.video_file = "bbb.mp4"
+
+
+
+
+
+a = Container("bbb.mp4")
+video_audio_std = dict([
+    ("DVBT-T", ["mpeg2","h264","aac","ac3","mp3"]),
+    ("ISBD-T", ["mpeg2","h264","aac"]),
+    ("ATSC", ["mpeg2","h264","ac3"]),
+    ("DTMB", ["avs","avs+","mpeg2","h264","dra","aac","mp2","mp3"])
+])
+a.broadcasting(video_audio_std)
+
+
